ab731431e23911341bc523b8fe1c29b32149d4ba
Zexuan <lzx213@sina.com>
2023-10-08 14:06:49 -0700

    Changed name of game

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Zexuan's game";
 
 document.title = gameName;
 
