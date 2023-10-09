0886c0f5c36d1ad2fff22b6e13b77123798959fe
Zexuan <lzx213@sina.com>
2023-10-09 12:11:35 -0700

    finished step 1

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.textContent = "🎁";
+
+app.append(button);
