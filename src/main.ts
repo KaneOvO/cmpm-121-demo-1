11fec2873a0c7cf1413a3a534eee3eac9f87fa6b
Zexuan <lzx213@sina.com>
2023-10-09 13:14:21 -0700

    finished step 2

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,24 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+//step1
 const button = document.createElement("button");
 button.textContent = "🎁";
 
 app.append(button);
+
+//step2
+let count: number = 0;
+const countText = document.createElement("presentCount");
+countText.innerHTML = `${count} present`;
+app.append(countText);
+function calculateCount() {
+  count++;
+  if (count <= 1) {
+    countText.innerHTML = `${count} present`;
+  } else {
+    countText.innerHTML = `${count} presents`;
+  }
+}
+
+button.addEventListener("click", calculateCount, false);
