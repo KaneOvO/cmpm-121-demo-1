8ad825867fa707da196a7526627e22abfb3d03c3
Zexuan <lzx213@sina.com>
2023-10-10 16:03:52 -0700

    finished step 6

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,11 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let growthRate: number = 0;
+const growthRateText = document.createElement("h2");
+growthRateText.innerHTML = `Current Growth Rate: ${growthRate}`;
+app.append(growthRateText);
+
 //step 1
 const button = document.createElement("button");
 button.textContent = "🎁";
@@ -50,7 +55,6 @@ if (!intervalID) {
 //step 4
 clearTimeout(intervalID);
 let previousTimeStamp: number;
-let growthRate: number = 0;
 function step(timestamp: number) {
   if (previousTimeStamp === undefined) {
     previousTimeStamp = timestamp;
@@ -69,27 +73,85 @@ function step(timestamp: number) {
 requestAnimationFrame(step);
 
 //step5
+// const lineBreak = document.createElement("br");
+// app.append(lineBreak);
+// const purchaseButton = document.createElement("button");
+// purchaseButton.disabled = true;
+// purchaseButton.innerHTML = `Purchase Growth Rate <br> Current Rate: ${growthRate}`;
+
+// setInterval(() => {
+//   if (count >= 10) {
+//     purchaseButton.disabled = false;
+//   } else {
+//     purchaseButton.disabled = true;
+//   }
+// }, 100);
+
+// function updatepurchaseButton() {
+//   if (count >= 10) {
+//     growthRate++;
+//     count -= 10;
+//     purchaseButton.innerHTML = `Purchase Growth Rate <br> Current Rate: ${growthRate}`;
+//   }
+// }
+// app.append(purchaseButton);
+// purchaseButton.addEventListener("click", updatepurchaseButton, false);
+// purchaseButton.addEventListener("click", updatepurchaseButton, false);
+
+//step 6
 const lineBreak = document.createElement("br");
 app.append(lineBreak);
-const purchaseButton = document.createElement("button");
-purchaseButton.disabled = true;
-purchaseButton.innerHTML = `Purchase Growth Rate <br> Current Rate: ${growthRate}`;
+
+const purchaseButton1 = document.createElement("button");
+purchaseButton1.innerHTML = `cost 10 unity, get 0.1 units/sec`;
+function updatepurchaseButton1() {
+  if (count >= 10) {
+    growthRate += 0.1;
+    count -= 10;
+    growthRateText.innerHTML = `Current Growth Rate: ${growthRate}`;
+  }
+}
+app.append(purchaseButton1);
+purchaseButton1.addEventListener("click", updatepurchaseButton1, false);
+
+const purchaseButton2 = document.createElement("button");
+purchaseButton2.innerHTML = `cost 100 unity, get 2 units/sec`;
+function updatepurchaseButton2() {
+  if (count >= 100) {
+    growthRate += 2;
+    count -= 100;
+    growthRateText.innerHTML = `Current Rate: ${growthRate}`;
+  }
+}
+app.append(purchaseButton2);
+purchaseButton2.addEventListener("click", updatepurchaseButton2, false);
+
+const purchaseButton3 = document.createElement("button");
+purchaseButton3.innerHTML = `cost 1000 unity, get 50 units/sec`;
+function updatepurchaseButton3() {
+  if (count >= 1000) {
+    growthRate += 50;
+    count -= 1000;
+    growthRateText.innerHTML = `Current Rate: ${growthRate}`;
+  }
+}
+app.append(purchaseButton3);
+purchaseButton3.addEventListener("click", updatepurchaseButton3, false);
 
 setInterval(() => {
   if (count >= 10) {
-    purchaseButton.disabled = false;
+    purchaseButton1.disabled = false;
   } else {
-    purchaseButton.disabled = true;
+    purchaseButton1.disabled = true;
+  }
+  if (count >= 100) {
+    purchaseButton2.disabled = false;
+  } else {
+    purchaseButton2.disabled = true;
+  }
+  if (count >= 1000) {
+    purchaseButton3.disabled = false;
+  } else {
+    purchaseButton3.disabled = true;
   }
 }, 100);
-
-function updatepurchaseButton() {
-  if (count >= 10) {
-    growthRate++;
-    count -= 10;
-    purchaseButton.innerHTML = `Purchase Growth Rate <br> Current Rate: ${growthRate}`;
-  }
-}
-app.append(purchaseButton);
-purchaseButton.addEventListener("click", updatepurchaseButton, false);
-purchaseButton.addEventListener("click", updatepurchaseButton, false);
